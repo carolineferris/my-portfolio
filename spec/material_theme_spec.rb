118b1df85f16f811ca35ff3e32645a00ba395c5b
Caroline <carolinegferris@gmail.com>
2018-03-28 13:57:50 -0500

    initial commit

--- spec/material_theme_spec.rb
+++ spec/material_theme_spec.rb
@@ -5,16 +5,9 @@ describe "Website Loads Up", type: :feature, js: true do
   it "has the page title" do
     visit '/'
 
-    expect(page).to have_content('Your awesome title')
-    expect(page).to have_content('Portfolio')
+    expect(page).to have_content('Hi There!')
+    expect(page).to have_content('Contact')
 
   end
 
-  it "renders the blog post page" do
-    visit '/'
-
-    click_link('/2017/12/04/hello-world/')
-    expect(page).to have_content('Hello World!')
-
-  end
 end
